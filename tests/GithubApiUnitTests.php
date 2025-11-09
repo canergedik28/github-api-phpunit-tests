220025d8573c5f37ee55e0db7796aa2245b105b1
canergedik <130908718+canergedik@users.noreply.github.com>
2025-11-09 23:05:48 +0300

    updated github api unit tests

---
+++ tests/GithubApiUnitTests.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpClient\HttpClient;
+
+
+
+class GithubApiUnitTests extends WebTestCase
+{
+    private  $client;
+
+    private $perPage = 100;
+
+    private $friendsList;
+    
+    private $followersList;
+
+    private $username = 'username';
+
+    private $userFriendsCount;
+
+    private $userFollowersCount;
+
+
+    public function setUp():void{
+
+        $this->client = HttpClient::create([
+            'base_uri' => 'https://api.github.com/',
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer github_pat_token',
+            ],
+            'timeout' => 10,
+        ]);
+
+    } 
+
+    public function userInfo(){
+            $response = $this->client->request('GET', 'users/'.$this->username);
+            $this->assertJson($response->getContent());
+            $data = $response->toArray();
+            $this->assertArrayHasKey('bio',$data);
+            $this->assertArrayHasKey('name',$data);
+            $this->assertArrayHasKey('followers',$data);
+            $this->assertArrayHasKey('company',$data);
+            $this->assertNotNull($data['bio']);
+            $this->assertArrayHasKey('following',$data);
+            $this->assertNotNull($data['bio']);
+            $this->assertTrue($data['followers'] > 0);
+            $this->assertTrue($data['following'] > 0);
+            $this->userFriendsCount = $data['following'];
+            $this->userFollowersCount = $data['followers'];
+            $this->assertSame($this->userFriendsCount,$data['following']);
+            $this->assertSame($this->userFollowersCount,$data['followers']);
+    }
+
+
+    public function userFollowers(){
+                $followersData  = [];
+                $this->assertTrue($this->userFollowersCount > 0);
+                $pagesCount = ceil($this->userFollowersCount / $this->perPage);
+                foreach (range(1,$pagesCount) as $page) {
+                    $response = $this->client->request('GET', 'users/'.$this->username.'/followers',['query' => ['per_page' => $this->perPage, 'page' =>$page]]);
+                    $this->assertJson($response->getContent());
+                    $this->assertSame(200,$response->getStatusCode());
+                    $data = $response->toArray();
+                    $data = array_column($data,'login');
+                    array_push($followersData,$data);
+                }
+                $newdata =  array_merge(...$followersData);
+                $this->followersList = $newdata;
+                $this->assertSame(count($this->followersList),$this->userFollowersCount);
+        }
+
+
+        public function userFriends(){
+                $friendsData  = [];
+                $this->assertTrue($this->userFriendsCount > 0);
+                $pagesCount = ceil($this->userFriendsCount / $this->perPage);
+                foreach (range(1,$pagesCount) as $page) {
+                    $response = $this->client->request('GET', 'users/'.$this->username.'/following',['query' => ['per_page' => $this->perPage, 'page' =>$page]]);
+                    $this->assertJson($response->getContent());
+                    $this->assertSame(200,$response->getStatusCode());
+                    $data = $response->toArray();
+                    $data = array_column($data,'login');
+                    array_push($friendsData,$data);
+                }
+                $newdata =  array_merge(...$friendsData);
+                $this->friendsList = $newdata;
+                $this->assertSame(count($this->friendsList),$this->userFriendsCount);
+        }
+
+        
+        public function unfollowList(){
+            $unfollowList = array_diff($this->friendsList,$this->followersList);
+            $this->assertNotNull($unfollowList);
+            $this->assertTrue(count($unfollowList)>0);
+        }
+
+        public function testGithubApi(){
+            $this->userInfo();
+            $this->userFollowers();
+            $this->userFriends();
+            $this->UnfollowList();
+        }
+
+}
+
+
